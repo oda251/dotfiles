b94da7f91e7a513e716bbcdeec0996adcacdbd4f
oda251 <69433701+oda251@users.noreply.github.com>
2026-01-31 14:52:06 +0900

    fix

--- zsh/.zsh/plugins.zsh
+++ zsh/.zsh/plugins.zsh
@@ -9,6 +9,9 @@ source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 # Starship prompt
 eval "$(starship init zsh)"
 
+# mise prompt
+eval "$(mise activate bash)"
+
 # Zellij terminal multiplexer
 if [[ -z "$ZELLIJ" ]]; then
     if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
