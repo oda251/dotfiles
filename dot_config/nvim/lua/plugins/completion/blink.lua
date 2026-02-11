0725006c46fe2622c6437402bf5299c0012cab2c
oda251 <69433701+oda251@users.noreply.github.com>
2026-02-12 02:48:56 +0900

    update

--- dot_config/nvim/lua/plugins/completion/blink.lua
+++ dot_config/nvim/lua/plugins/completion/blink.lua
@@ -20,7 +20,10 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
+    keymap = {
+      preset = 'default',
+      ['<Tab>'] = { 'accept', 'fallback' },
+    },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
