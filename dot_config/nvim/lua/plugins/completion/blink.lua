168e14799805d53075255b26cd93c2e02f3e0d9c
oda251 <69433701+oda251@users.noreply.github.com>
2026-02-12 03:15:07 +0900

    fix

--- dot_config/nvim/lua/plugins/completion/blink.lua
+++ dot_config/nvim/lua/plugins/completion/blink.lua
@@ -22,7 +22,6 @@ return {
     -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
       preset = 'default',
-      ['<Tab>'] = { 'accept', 'fallback' },
       ['<C-l>'] = { 'accept', 'fallback' },
     },
 
