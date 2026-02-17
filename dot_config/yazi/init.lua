d6498fa3339144d4c135b6204927f1e0196b3e24
oda251 <69433701+oda251@users.noreply.github.com>
2026-02-17 20:58:03 +0900

    update

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -2,4 +2,7 @@ require("git"):setup({
     order = 1500,
 })
 
-require("starship"):setup()
+require("starship"):setup({
+    config_file = "~/.config/starship_yazi.toml",
+    hide_flags = true,
+})
