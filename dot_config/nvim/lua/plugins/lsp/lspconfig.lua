82a389e23b29c0f04e3fbf20c0953cefd0916ccf
oda251 <69433701+oda251@users.noreply.github.com>
2026-03-08 14:16:19 +0900

    upd

--- dot_config/nvim/lua/plugins/lsp/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -3,26 +3,22 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   cmd = { "LspInfo", "LspInstall", "LspStart", "LspStop", "Mason", "MasonInstall", "MasonUpdate" },
   dependencies = {
-    { "williamboman/mason.nvim", config = true },
-    "williamboman/mason-lspconfig.nvim",
+    { "mason-org/mason.nvim", opts = {} },
+    "mason-org/mason-lspconfig.nvim",
     "saghen/blink.cmp",
   },
   config = function()
-    local capabilities = require("blink.cmp").get_lsp_capabilities()
-    local lspconfig = require("lspconfig")
+    vim.lsp.config("*", {
+      capabilities = require("blink.cmp").get_lsp_capabilities(),
+    })
 
     require("mason-lspconfig").setup({
       ensure_installed = { "lua_ls", "vtsls", "tailwindcss", "clangd", "rust_analyzer", "pyright" },
-      handlers = {
-        function(server_name)
-          lspconfig[server_name].setup({ capabilities = capabilities })
-        end,
-      },
     })
 
     -- oxlint LSP (mise管理、mason外)
     if vim.fn.executable("oxlint") == 1 then
-      lspconfig.oxlint.setup({ capabilities = capabilities })
+      vim.lsp.enable("oxlint")
     end
   end,
 }
